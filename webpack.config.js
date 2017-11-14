c5b11944e5b55b43f65e2c608262ddc80adf8540
Lincoln de Sousa <lincoln@comum.org>
2017-11-14 18:04:51 -0500

    Remove /assets in index.html & webpack.config.js to work with GH pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, "docs"),
-    publicPath: "/assets/",
+    publicPath: "/",
     filename: "bundle.js"
   },
   module: {
