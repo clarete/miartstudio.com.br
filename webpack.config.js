2dae4dd8295767504ef46dd08e53651689ee98f7
Lincoln de Sousa <lincoln@comum.org>
2017-11-14 17:49:43 -0500

    Use `/docs` instead of `/build` as target to be used with Github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "docs"),
     publicPath: "/assets/",
     filename: "bundle.js"
   },
